8e5f878e060e55b9c3de675ebd14f75159bf0698
Marios <xrteach@gmail.com>
2024-08-31 01:31:17 +0300

    feat: AccountServiceTests & JwtServiceTests

--- src/test/java/xyz/mariosm/bank/BankApplicationTests.java
+++ src/test/java/xyz/mariosm/bank/BankApplicationTests.java
@@ -2,8 +2,6 @@ package xyz.mariosm.bank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class BankApplicationTests {
